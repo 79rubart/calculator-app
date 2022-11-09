da12dff0c919db59251b90ec4e46e0fdfa345505
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-11-08 23:17:19 -0300

    other operations added, need fixes

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ var visor = document.getElementById("visor");
 var resetBtn = document.getElementById("reset");
   // Get all the buttons to an Array
 var buttons = document.getElementsByClassName("key");
+  // Get all operators button
+var plus = document.getElementById("plus");
+var subtract = document.getElementById("subtract");
+var multiply = document.getElementById("multiply");
+var divide = document.getElementById("divide");
 
 function numbersAndOperators() {
     // Add click event listener to all button elements and insert their inner text as value to the text field
@@ -21,14 +26,32 @@ function numbersAndOperators() {
     })
 }
 
-equals.addEventListener('click', function(){
-    if (visor.value.split('+')){
-        visor.value = visor.value.split('+').reduce((a,c) => a + Number(c), 0);
-    }
-    //if (visor.value.split('-')){
-        //visor.value = visor.value.split('-').reduce((a,c) => a - Number(c), 0);
-    //}
+plus.addEventListener('click', function(){
+  equals.addEventListener('click', function(){
+    var valAdd = visor.value.split('+');
+    visor.value = valAdd.reduce((a,c) => a + Number(c), 0);
+  });
 });
 
+subtract.addEventListener('click', function(){
+  equals.addEventListener('click', function(){
+    var valSub = visor.value.split('-');
+    visor.value = valSub.reduce((a,c) => a - Number(c), 0);
+  });
+});
+
+multiply.addEventListener('click', function(){
+  equals.addEventListener('click', function(){
+    var valMul = visor.value.split('*');
+    visor.value = valMul.reduce((a,c) => a * Number(c), 0);
+  });
+});
+
+divide.addEventListener('click', function(){
+  equals.addEventListener('click', function(){
+    var valDiv = visor.value.split('/');
+    visor.value = valDiv.reduce((a,c) => a / Number(c), 0);
+  });
+});
 
 document.addEventListener("DOMContentLoaded", numbersAndOperators);
