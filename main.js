3ab283af1c9f90f79ef30353853ce4f9a15073f2
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-11-11 20:36:50 -0300

    major style changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var equalsBtn = document.querySelector('#equals');
 var visor = document.getElementById("visor");
+var visor2 = document.getElementById("visor2");
 var resetBtn = document.getElementById("reset");
 var buttons = document.getElementsByClassName("key");
 var plus = document.getElementById("plus");
@@ -15,15 +16,22 @@ function numbersAndOperators() {
       })
     })
     resetBtn.addEventListener("click", () => {
-      visor.value = null
+      visor.value = null;
+      visor2.value = null;
+    })
+
+    plus.addEventListener('click', function(){
+      visor2.value = visor.value;
+      visor.value = null;
     })
 
     equalsBtn.addEventListener('click', function(){
-      if (visor.value.split('-')){
-        visor.value = visor.value.split('-').slice(1).reduce((a, c) => parseFloat(a) - parseFloat(c), (visor.value.split('-')[0]));
+      if (visor2.value.split('-')){
+        visor.value = visor2.value.split('-').slice(1).reduce((a, c) => parseFloat(a) - parseFloat(c), (visor.value.split('-')[0]));
       }
-      if (visor.value.split('+')){
-        visor.value = visor.value.split('+').slice(1).reduce((a, c) => parseFloat(a) + parseFloat(c), (visor.value.split('+')[0]));
+      if (visor2.value.split('+')){
+        visor.value = visor2.value.split('+').slice(1).reduce((a) => parseFloat(a) + parseFloat(visor.value), (visor2.value.split('+')[0]));
+        visor2.value = null;
       }
       if (visor.value.split('*')){
         visor.value = visor.value.split('*').slice(1).reduce((a, c) => parseFloat(a) * parseFloat(c), (visor.value.split('*')[0]));
